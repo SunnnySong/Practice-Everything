6e6fa4ee78a9a8a8bb339d6e23da10712a364857
SunnnySong <tjsghktjswls@naver.com>
2022-10-18 00:02:32 +0900

    Create new folder

---
+++ Allen-Project/4. Calendar/FSCalendar/FSCalendar_practice/FSCalendar_practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FSCalendar_practice
+//
+//  Created by 송선진 on 2022/10/17.
+//
+
+import UIKit
+import FSCalendar
+
+class ViewController: UIViewController {
+
+    var calendar: FSCalendar = {
+        let cal = FSCalendar()
+        return cal
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
