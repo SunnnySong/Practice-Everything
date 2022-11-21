0bfd7e50d3dc881af2b419651182bb2d852d77ad
SunnnySong <tjsghktjswls@naver.com>
2022-11-21 18:10:27 +0900

    Fix: Fixed the custom TabBar

--- Allen-Project_Sunny/AllenProject/AllenProject/Views/TabBar/CustomTabBar.swift
+++ Allen-Project_Sunny/AllenProject/AllenProject/Views/TabBar/CustomTabBar.swift
@@ -6,22 +6,133 @@
 //
 
 import UIKit
+import SnapKit
 
 class CustomTabBar: UITabBar {
     
+    public var middleBtnActionHandler:() -> () = {}
+    
+    private var shapeLayer: CALayer?
+    
+    let middleButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = UIColor(hex: "5294F9")
+        
+        var titleAttr = AttributedString.init("로또 QR")
+        titleAttr.font = .systemFont(ofSize: 13, weight: .bold)
+        config.attributedSubtitle = titleAttr
+        config.titleAlignment = .center
+        
+        let image = UIImage(systemName: "qrcode")
+        config.image = image
+        config.imagePadding = 10
+        config.imagePlacement = .top
+        
+        return UIButton(configuration: config)
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        let tabBarAppearance = UITabBarAppearance()
-        tabBarAppearance.configureWithDefaultBackground()
-        tabBarAppearance.backgroundColor = UIColor(hex: "202632")
 
-        self.standardAppearance = tabBarAppearance
-        self.scrollEdgeAppearance = tabBarAppearance
-        self.tintColor = .white
+        setupTabBar()
+        setupMiddleBtn()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+//    override func draw(_ rect: CGRect) {
+//        addShape()
+//    }
+    
+    /*
+     - UITabBar 범위 이상으로 버튼이 있을 때, UITabBar 범위에 벗어난 버튼도 눌릴 수 있도록 hitTest()
+     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        guard !clipsToBounds && !isHidden && alpha > 0 else { return nil }
+        for member in subviews.reversed() {
+            let subPoint = member.convert(point, from: self)
+            guard let result = member.hitTest(subPoint, with: event) else { continue }
+            return result
+        }
+        return nil
+     }
+     아래코드와 달리 해당 코드는 tabBar에서 벗어나는 middleButton 만 해당이 됌. 
+     */
+    
+    // middleButton 전역 touch 활성화
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+      guard !clipsToBounds && !isHidden && alpha > 0 else { return nil }
+
+      return self.middleButton.frame.contains(point) ? self.middleButton : super.hitTest(point, with: event)
+    }
+    
+    private func setupTabBar() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithDefaultBackground()
+        tabBarAppearance.backgroundColor = UIColor(hex: "202632")
+        
+        self.standardAppearance = tabBarAppearance
+        self.scrollEdgeAppearance = tabBarAppearance
+        self.itemSpacing = 130
+        self.tintColor = .white
+    }
+    
+    private func setupMiddleBtn() {
+        self.addSubview(middleButton)
+        
+        middleButton.frame = CGRect(x: (self.bounds.width / 2)-(80 / 2), y: -40, width: 80, height: 80)
+        middleButton.clipsToBounds = true
+        middleButton.layer.cornerRadius = middleButton.frame.width / 2
+        middleButton.addTarget(self, action: #selector(middleBtnAction), for: .touchUpInside)
+    }
+    
+    @objc func middleBtnAction() {
+        middleBtnActionHandler()
+        print("tapped")
+    }
+    
+    // 곡선 TabBar
+    private func addShape() {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = createPath()
+        shapeLayer.strokeColor = UIColor.clear.cgColor
+        shapeLayer.fillColor = UIColor(hex: "202632").cgColor
+        shapeLayer.lineWidth = 0
+        
+        if let oldShapeLayer = self.shapeLayer {
+            self.layer.replaceSublayer(oldShapeLayer, with: shapeLayer)
+        } else {
+            self.layer.insertSublayer(shapeLayer, at: 0)
+        }
+        self.shapeLayer = shapeLayer
+        self.tintColor = UIColor.white
+        self.unselectedItemTintColor = UIColor.gray
+        self.setupMiddleBtn()
+        }
+    
+    private func createPath() -> CGPath {
+        let f = CGFloat(80 / 2.0) + 5
+        let h = frame.height
+        let w = frame.width
+        let halfW = frame.width/2.0
+        let r = CGFloat(0)
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        
+        path.addLine(to: CGPoint(x: halfW-f-(r/2.0), y: 0))
+        
+        path.addQuadCurve(to: CGPoint(x: halfW-f, y: (r/2.0)), controlPoint: CGPoint(x: halfW-f, y: 0))
+        
+        path.addArc(withCenter: CGPoint(x: halfW, y: (r/2.0)), radius: f, startAngle: .pi, endAngle: 0, clockwise: false)
+        
+        path.addQuadCurve(to: CGPoint(x: halfW+f+(r/2.0), y: 0), controlPoint: CGPoint(x: halfW+f, y: 0))
+        
+        path.addLine(to: CGPoint(x: w, y: 0))
+        path.addLine(to: CGPoint(x: w, y: h))
+        path.addLine(to: CGPoint(x: 0.0, y: h))
+        
+        return path.cgPath
+    }
+    
 }
--- Allen-Project_Sunny/AllenProject/AllenProject/Views/TabBar/TabBarViewController.swift
+++ Allen-Project_Sunny/AllenProject/AllenProject/Views/TabBar/TabBarViewController.swift
@@ -8,28 +8,45 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setValue(CustomTabBar(frame: tabBar.frame), forKey: "tabBar")
-        
-        setupTabBar()
         tabBarConfigure()
+        setupTabBar()
     }
     
     private func tabBarConfigure() {
+        setValue(CustomTabBar(frame: tabBar.frame), forKey: "tabBar")
+        
+        delegate = self
     }
     
     private func setupTabBar() {
         
         let calendar = naviController(image: UIImage(systemName: "calendar.badge.plus"), title: "달력", rootViewController: DateViewController())
         let home = naviController(image: UIImage(systemName: "house"), title: "홈", rootViewController: HomeViewController())
-        let lotto = naviController(image: UIImage(systemName: "qrcode"), title: "로또 QR", rootViewController: LottoViewController())
         let chart = naviController(image: UIImage(systemName: "chart.bar"), title: "차트", rootViewController: ChartViewController())
         let num = naviController(image: UIImage(systemName: "number.circle"), title: "번호 추첨", rootViewController: RandomMoneyViewController())
         
-        self.viewControllers = [ calendar, home, lotto, chart, num ]
+        self.viewControllers = [ calendar, home, UIViewController() , chart, num ]
+        
+        /*
+         참조 : https://keyhan-kam.medium.com/tabbar-with-raised-middle-button-swift-132ab62c7911
+         customTabBar.middleBtnActionHandler = {
+            print("moving")
+            // self.navigationController?.pushViewController(LottoViewController(), animated: true)
+            self.selectedIndex = 2
+         }
+         이런식으로 middleBtn을 클릭했을시 action을 처리하려 했는데, CustomTabBar까지는 작동이 되는데 TabBarViewController로 넘어오지 않았음.
+         이유는 왜인지 모르겠지만 아래와 같이 실행해줘야 가능.
+         */
+        guard let tabBar = self.tabBar as? CustomTabBar else { return }
+        tabBar.middleBtnActionHandler = {
+            print("clicked")
+//            self.selectedIndex = 2
+            self.navigationController?.pushViewController(LottoViewController(), animated: true)
+        }
     }
     
     func naviController(image: UIImage!, title: String, rootViewController: UIViewController) -> UINavigationController {
@@ -38,5 +55,25 @@ class TabBarViewController: UITabBarController {
         navi.tabBarItem.title = title
         navi.tabBarItem.image = image.withTintColor(.white)
         return navi
+    }
+}
+
+extension TabBarViewController: UITabBarControllerDelegate {
+    
+    /*
+     middleButton은 tabBar item[2]의 구역에 존재하지만, 포함되지 않는 부분도 존재.
+     hitTest로 middleButton 전역이 눌릴 수 있도록 구현했으니, middleButton이 아닌 tabBar item[2]의 부분이 눌리지 않도록 tabBar item[2]를 비활성화 시킴.(select을 false로)
+     */
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        guard let selectedIndex = tabBarController.viewControllers?.firstIndex(of: viewController) else {
+            return true
+        }
+        
+        if selectedIndex == 2 {
+            return false
+        }
+        return true
     }
 }
